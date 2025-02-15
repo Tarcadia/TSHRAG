6cbe2500b4cd591e973aa192375d35208d05c73e
Tarcadia <tarcadia@qq.com>
2025-02-16 03:02:42 +0800

    Update Mdb implementation

--- tshrag/core/data/mdb.py
+++ tshrag/core/data/mdb.py
@@ -6,6 +6,7 @@ import json
 import shutil
 
 from pathlib import Path
+from contextlib import contextmanager
 from dataclasses import asdict
 from typing import List, Generator
 
@@ -24,88 +25,149 @@ from ...util.consts import ENCODING
 
 class Mdb:
 
+    MDB_FILE_LOCK = PATH_LOCK
     MDB_FILE_MID_SEPARATOR = "-"
     MDB_FILE_INDEX = "index.json"
     MDB_FILE_DATA = "%s.jsonl"
     MDB_TIME_ACCURACY = 12
     MDB_CONFIG_SESSION = "mdb"
 
-    def __init__(self, root: Path, time_accuracy = MDB_TIME_ACCURACY, timeout = TIMEOUT, encoding = ENCODING, config: Config = None):
+    def __init__(
+        self, root: Path,
+        time_accuracy = MDB_TIME_ACCURACY,
+        timeout = TIMEOUT,
+        encoding = ENCODING,
+        config: Config = None
+    ):
         self._root = Path(root)
         self._time_accuracy = time_accuracy
         self._timeout = timeout
         self._encoding = encoding
         if not config is None:
             config.pick_to(Mdb.MDB_CONFIG_SESSION, self)
-    
-    def _get_metric_path(self, id: MetricId) -> Path:
-        _subpath = Mdb.MDB_FILE_MID_SEPARATOR.join(id.keys())
-        return self._root / _subpath
+        
+        self._root.mkdir(parents=True, exist_ok=True)
 
-    def _get_metric_index(self, id: MetricId) -> Path:
+
+    @contextmanager
+    def _metric_lock(self, id: MetricId):
+        with Lock(self._get_metric_lock_file(id), timeout=self._timeout) as _l:
+            yield _l
+
+
+    def _get_metric_fsname(self, id: MetricId) -> str:
+        return Mdb.MDB_FILE_MID_SEPARATOR.join(id.keys())
+
+
+    def _get_metric_path(self, id: MetricId) -> Path:
+        return self._root / self._get_metric_fsname(id)
+
+
+    def _get_metric_lock_file(self, id: MetricId) -> Path:
+        return self._root / (self._get_metric_fsname(id) + Mdb.MDB_FILE_LOCK)
+
+
+    def _get_metric_index_file(self, id: MetricId) -> Path:
         return self._get_metric_path(id) / Mdb.MDB_FILE_INDEX
-    
-    def _get_metric_file(self, id: MetricId, time: Time) -> Path:
-        _file = Mdb.MDB_FILE_DATA % time[:self._time_accuracy]
+
+
+    def _get_metric_data_file(self, id: MetricId, time: Time) -> Path:
+        _file = Mdb.MDB_FILE_DATA % str(time)[:self._time_accuracy]
         return self._get_metric_path(id) / _file
-    
-    def _glob_metric_file(self, id: MetricId) -> Generator[Path, None, None]:
+
+
+    def _glob_metric_index_file(self) -> Generator[Path, None, None]:
+        _file = f"*/{Mdb.MDB_FILE_INDEX}"
+        yield from self._root.glob(_file)
+
+
+    def _glob_metric_data_file(self, id: MetricId) -> Generator[Path, None, None]:
         _file = Mdb.MDB_FILE_DATA % "*"
-        return self._get_metric_path(id).glob(_file)
-    
-    def _range_metric_file(self, id: MetricId, start_time: Time, end_time: Time) -> Generator[Path, None, None]:
-        for _file in self._glob_metric_file(id):
-            if not start_time <= Time(_file.stem) <= end_time:
+        yield from self._get_metric_path(id).glob(_file)
+
+
+    def _range_metric_data_file(self, id: MetricId, start_time: Time, end_time: Time) -> Generator[Path, None, None]:
+        _start_time = Time(str(start_time)[:self._time_accuracy])
+        _end_time = Time(str(end_time)[:self._time_accuracy])
+        for _file in self._glob_metric_data_file(id):
+            if not _start_time <= Time(_file.stem) <= _end_time:
                 continue
             yield _file
-    
-    def _get_metric_lock(self, id: MetricId) -> Path:
-        return self._get_metric_path(id) / PATH_LOCK
+
 
     def create(self, metric: Metric) -> bool:
-        _mpath = self._get_metric_path(metric.id)
-        if _mpath.exists():
-            return False
-        _mpath.mkdir(parents=True, exist_ok=True)
-        with Lock(self._get_metric_lock(metric.id), timeout=TIMEOUT):
-            with self._get_metric_index(metric.id).open("w") as fp:
+        with self._metric_lock(metric.id):
+            _mpath = self._get_metric_path(metric.id)
+            if _mpath.exists():
+                return False
+            _mpath.mkdir(parents=True, exist_ok=True)
+            with self._get_metric_index_file(metric.id).open("w") as fp:
                 json.dump(asdict(metric), fp, indent=4)
-        return True
-    
+            return True
+
+
     def delete(self, id: MetricId) -> bool:
-        _mpath = self._get_metric_path(id)
-        if not _mpath.exists():
-            return False
-        shutil.rmtree(_mpath)
-        return True
-    
+        id = MetricId(id)
+        with self._metric_lock(id):
+            _mpath = self._get_metric_path(id)
+            if not _mpath.exists():
+                return False
+            shutil.rmtree(_mpath)
+            return True
+
+
+    def get(self, id: MetricId) -> Metric:
+        id = MetricId(id)
+        metric = None
+        try:
+            with self._get_metric_index_file(id).open("r") as fp:
+                metric = Metric(**json.load(fp))
+        except:
+            # TODO: Log error
+            pass
+        return metric
+
+
     def list(self) -> List[Metric]:
-        _ids = []
-        for _subpath in self._root.iterdir():
-            if not _subpath.is_dir():
+        metrics = []
+        for _index in self._glob_metric_index_file():
+            try:
+                with _index.open("r") as fp:
+                    _metric = Metric(**json.load(fp))
+            except:
                 continue
-            _ids.append(MetricId(*_subpath.name.split(Mdb.MDB_FILE_MID_SEPARATOR)))
-        return _ids
+            if self._get_metric_fsname(_metric.id) == _index.parent.name:
+                metrics.append(_metric)
+        return metrics
+
 
     def read(self, id: MetricId, start_time: Time, end_time: Time) -> List[MetricEntry]:
-        _entries = []
-        with Lock(self._get_metric_lock(id), timeout=self._timeout):
-            _index = self._get_metric_index(id)
-            if not _index.exists():
-                return _entries
-            for _file in self._range_metric_file(id, start_time, end_time):
+        id = MetricId(id)
+        entries = []
+        for _file in self._range_metric_data_file(id, start_time, end_time):
+            try:
                 with _file.open("r") as fp:
                     for _line in fp:
-                        if not _line:
+                        try:
+                            _entry = MetricEntry(**json.loads(_line))
+                        except:
                             continue
-                        _entry = MetricEntry(**json.loads(_line))
                         if start_time <= _entry.time <= end_time:
-                            _entries.append(_entry)
-        return _entries
+                            entries.append(_entry)
+            except:
+                continue
+        entries.sort(key=lambda e : e.time)
+        return entries
 
-    def write(self, entry: MetricEntry) -> bool:
-        with Lock(self._get_metric_lock(entry.id), timeout=self._timeout):
-            with self._get_metric_file(entry.id, entry.time).open("a") as fp:
-                json.dump(asdict(entry), fp)
-                fp.write("\n")
+
+    def write(self, id: MetricId, entry: MetricEntry) -> bool:
+        id = MetricId(id)
+        try:
+            with self._metric_lock(id):
+                with self._get_metric_data_file(id, entry.time).open("a") as fp:
+                    fp.write(json.dumps(asdict(entry), default=lambda value : str(value)))
+                    fp.write("\n")
+            return True
+        except:
+            return False
 
